a4408151a1ffa3ea84070a4862241e9133beb463
Bo-Deng <boyang.deng@gmail.com>
2016-09-14 13:42:32 -0500

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpOFFICIAL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpOFFICIAL.java
@@ -1,10 +1,14 @@
-package com.qualcomm.ftcrobotcontroller.opmodes;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 /**
  * Created by Bo on 9/12/2016.
  */
+@TeleOp(name = "TeleOpOfficial", group = "TeleOp")
 public class TeleOpOFFICIAL extends OpMode {
     public void init() {
 
@@ -12,7 +16,8 @@ public class TeleOpOFFICIAL extends OpMode {
 
     public void loop() {
 
-        double g_ = gamepad1.left_stick_y;
+        double g1_leftY = gamepad1.left_stick_y;
+        double g2_leftY = gamepad1.right_stick_y;
 
     }
 }
